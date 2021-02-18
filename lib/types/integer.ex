c780b051c542268d1346f5b345b12319e22dcb33
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-02-18 17:16:26 +0000

    add Integer comparative funcs

--- lib/types/integer.ex
+++ lib/types/integer.ex
@@ -1,4 +1,6 @@
 defmodule DryValidation.Types.Integer do
+  alias DryValidation.Types.Func
+
   def cast(value) when is_number(value), do: value
 
   def cast(value) when is_binary(value) do
@@ -12,4 +14,28 @@ defmodule DryValidation.Types.Integer do
 
   def valid?(value) when is_number(value), do: true
   def valid?(_value), do: false
+
+  def greater_than(value) do
+    %Func{
+      fn: fn v -> v > value end
+    }
+  end
+
+  def greater_than_or_equal(value) do
+    %Func{
+      fn: fn v -> v >= value end
+    }
+  end
+
+  def less_than(value) do
+    %Func{
+      fn: fn v -> v < value end
+    }
+  end
+
+  def less_than_or_equal(value) do
+    %Func{
+      fn: fn v -> v <= value end
+    }
+  end
 end
--- test/types/integer_test.exs
+++ test/types/integer_test.exs
@@ -38,4 +38,54 @@ defmodule DryValidation.Types.IntegerTest do
       end
     end
   end
+
+  describe "#greater_than" do
+    it "returns a Func struct" do
+      assert %Types.Func{} = Types.Integer.greater_than(1)
+    end
+
+    it "works as expected" do
+      assert Types.Integer.greater_than(1) |> Types.Func.call(2) == true
+      assert Types.Integer.greater_than(1) |> Types.Func.call(1) == false
+      assert Types.Integer.greater_than(1) |> Types.Func.call(0) == false
+    end
+  end
+
+  describe "#greater_than_or_equal" do
+    it "returns a Func struct" do
+      assert %Types.Func{} = Types.Integer.greater_than_or_equal(1)
+    end
+
+    it "works as expected" do
+      assert Types.Integer.greater_than_or_equal(1) |> Types.Func.call(2) == true
+      assert Types.Integer.greater_than_or_equal(1) |> Types.Func.call(1) == true
+      assert Types.Integer.greater_than_or_equal(1) |> Types.Func.call(0) == false
+    end
+  end
+
+  describe "#less_than" do
+    it "returns a Func struct" do
+      assert %Types.Func{} = Types.Integer.less_than(1)
+    end
+
+    it "works as expected" do
+      assert Types.Integer.less_than(1) |> Types.Func.call(-10) == true
+      assert Types.Integer.less_than(1) |> Types.Func.call(0) == true
+      assert Types.Integer.less_than(1) |> Types.Func.call(2) == false
+      assert Types.Integer.less_than(1) |> Types.Func.call(1) == false
+    end
+  end
+
+  describe "#less_than_or_equal" do
+    it "returns a Func struct" do
+      assert %Types.Func{} = Types.Integer.less_than_or_equal(1)
+    end
+
+    it "works as expected" do
+      assert Types.Integer.less_than_or_equal(1) |> Types.Func.call(-10) == true
+      assert Types.Integer.less_than_or_equal(1) |> Types.Func.call(0) == true
+      assert Types.Integer.less_than_or_equal(1) |> Types.Func.call(2) == false
+      assert Types.Integer.less_than_or_equal(1) |> Types.Func.call(1) == true
+    end
+  end
 end
